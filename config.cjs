17adad237d0fdc3bbd05410afd5f17b373278bb9
ksparrow602 <ksparrow602@gmail.com>
2025-05-29 22:02:03 +0000

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -4,7 +4,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "JOEL~XMD~WZj0KrAR",
+  SESSION_ID: process.env.SESSION_ID || "JOEL-XMD~ZAYRFRII#JFOaU8xfIAJqZdMJszvLIiyf70sCh_WirgrcZxqbCuY",
   PREFIX: process.env.PREFIX || '.',
 //let's add menu captions by lord joel
    BOT_NAME: process.env.BOT_NAME || "ᴊᴏᴇʟ xᴍᴅ ʙᴏᴛ ᴠ¹⁰",
